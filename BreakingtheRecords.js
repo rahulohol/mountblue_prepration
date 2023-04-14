1951ee61a5a4b501112b377b6beb08671d86838b
Rahul Ohol <rahulohol01@gmail.com>
2023-04-14 22:46:50 +0530

    BreakingtheRecords

---
+++ BreakingtheRecords.js
@@ -0,0 +1,20 @@
+function breakingRecords(scores) {
+    // Write your code here
+    let currentHigh=scores[0];
+     let currentLow=scores[0];
+    let lowCount=0;
+    let highCount=0;
+    
+    for(let i=0; i<scores.length; i++){
+        if(currentHigh<scores[i]){
+            currentHigh=scores[i];
+            highCount++;
+        }
+         if(currentLow>scores[i]){
+            currentLow=scores[i];
+            lowCount++;
+        }
+    }
+    return [highCount,lowCount];
+
+}
